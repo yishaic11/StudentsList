e91cf44324dbd7df5055f6d6479266ac8846c209
yishaic11 <yishai.chen28@gmail.com>
2026-01-16 16:40:15 +0200

    Added code to link the adapter and recyclerview to main activity (students list), also skeleton for future code

--- app/src/main/java/com/example/studentslist/MainActivity.kt
+++ app/src/main/java/com/example/studentslist/MainActivity.kt
@@ -1,20 +1,73 @@
 package com.example.studentslist
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.studentslist.adapters.StudentsAdapter
+import com.example.studentslist.models.Student
+import com.example.studentslist.repositories.StudentsRepository
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: StudentsAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var noStudentsMessage: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        recyclerView = findViewById<RecyclerView>(R.id.students_list)
+        recyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        noStudentsMessage = findViewById(R.id.no_students_message)
+
+        initRecyclerView()
+        setNoStudentsMessage()
+
+        // TODO: Link click event on + button to new student form activity
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initRecyclerView()
+        setNoStudentsMessage()
+    }
+
+    private fun initRecyclerView() {
+        adapter = StudentsAdapter(
+            StudentsRepository.students,
+            onStudentItemClick = { student: Student ->
+                // TODO: Link click event to student details form activity
+            },
+            onCheckBoxClick = { student, isChecked ->
+                student.isPresent = isChecked
+            })
+        recyclerView.adapter = adapter
+    }
+
+    private fun setNoStudentsMessage() {
+        /* This method updates the visibility of the recycler view
+           (responsible to display students list) and the no-students-message
+           if there are no students
+        */
+        val hasStudents = StudentsRepository.students.isNotEmpty()
+
+        recyclerView.visibility = if (hasStudents) View.VISIBLE else View.GONE
+        noStudentsMessage.visibility = if (hasStudents) View.GONE else View.VISIBLE
     }
 }
