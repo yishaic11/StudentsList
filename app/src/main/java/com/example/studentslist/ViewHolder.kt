575bbc55360a8cf478d5966f9a20c95cf2471485
yishaic11 <yishai.chen28@gmail.com>
2026-01-16 17:00:02 +0200

    Renamed is_present -> is_checked

--- app/src/main/java/com/example/studentslist/ViewHolder.kt
+++ app/src/main/java/com/example/studentslist/ViewHolder.kt
@@ -10,5 +10,5 @@ class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val idView = itemView.findViewById<TextView>(R.id.students_list_item_id)
     val nameView = itemView.findViewById<TextView>(R.id.students_list_item_name)
     val avatarView = itemView.findViewById<ImageView>(R.id.students_list_item_avatar)
-    val isCheckedView = itemView.findViewById<CheckBox>(R.id.students_list_item_is_present)
+    val isCheckedView = itemView.findViewById<CheckBox>(R.id.students_list_item_is_checked)
 }
